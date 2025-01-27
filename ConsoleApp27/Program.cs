8e33d3727f9015421c9b2cc52f85cbc726d6f730
MAkzezzz <MAkzezzz@DESKTOP-B3A2T14>
2025-01-27 21:20:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public interface ISelectable
+{
+    void OnSelect();
+}
+public interface IUpdatable
+{
+    void Refresh();
+}
+
+public class Screen : ISelectable, IUpdatable
+{
+    public void OnSelect()
+    {
+        Console.WriteLine("Screen selected.");
+    }
+
+    public void Refresh()
+    {
+        Console.WriteLine("Screen refreshed.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Screen screen = new Screen();
+
+        screen.OnSelect();
+
+        screen.Refresh();
+    }
+}
